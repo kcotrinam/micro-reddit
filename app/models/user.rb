98431f702020433d8daae1bdbd65136bc5ca5caf
kevin Cotrina <kcotrinam92@gmail.com>
2020-11-12 12:46:28 -0500

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, :password, :email, presence: { message: 'Must be given please '}
-  validates :name, :email, uniqueness: { message: 'Name or email already used'}
+  validates :name, :password, :email, presence: { message: 'Must be given please ' }
+  validates :name, :email, uniqueness: { message: 'Name or email already used' }
 
   has_many :posts
   has_many :comments
