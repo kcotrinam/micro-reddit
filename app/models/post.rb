0b73fb1104317cbc466f8da8dd1c08be09c85b8f
kevin Cotrina <kcotrinam92@gmail.com>
2020-11-12 11:42:41 -0500

    Add validations for post and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :body, :url, :user_id, presence: true
+
+  
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :password, :email, presence: true
+  validates :name, :password, :email, presence: { message: 'Must be given please '}
+  validates :name, :email, uniqueness: { message: 'Name or email already used'}
+
   has_many :posts
   has_many :comments
 end
